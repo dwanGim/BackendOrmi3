ea38913e7fb055a75ae658bfe1fd0005c36202b2
dwanGim <nugoory20@gmail.com>
2023-11-17 14:19:16 +0900

    231117JWT

---
+++ Django/1116/mysite/blog/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DeleteView, UpdateView, DetailView, CreateView
+from .models import Post
+from .forms import PostForm
+from django.urls import reverse_lazy
+
+
+class PostListView(ListView):
+    model = Post
+
+post_list = PostListView.as_view()
+
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('blog:post_list')
+    template_name = 'blog/form.html'
+
+post_new = PostCreateView.as_view()
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+post_detail = PostDetailView.as_view()
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('blog:post_list')
+    template_name = 'blog/form.html'
+
+post_edit = PostUpdateView.as_view()
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:post_list')
+
+post_delete = PostDeleteView.as_view()
---
+++ Django/1116/mysite/tutorialdjango/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic.base import RedirectView
+
+urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='blog:post_list'), name='root'),
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
