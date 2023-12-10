440482a33a9344822bdf05e6757889bc64230277
dwanGim <nugoory20@gmail.com>
2023-12-10 20:38:39 +0900

    231210

--- Django/1209/blog/serializers.py
+++ Django/1209/blog/serializers.py
@@ -1,10 +1,21 @@
-# blog > serializers.py
+
 from rest_framework import serializers
 
+
 class PostSerializer(serializers.Serializer):
-    '''
-    아래 max_length와 같은 값은 유효성 검사를 위한 제약조건입니다.
-    데이터가 11자여도 애러이고, 들어온 데이터가 11자여도 애러입니다.
-    '''
-    title = serializers.CharField(max_length=10)
-    content = serializers.CharField(max_length=100)
+    title = serializers.CharField(
+        max_length=10, 
+        error_messages={
+            'max_length': '제목은 10자를 초과할 수 없습니닷!',
+            'blank': '제목은 비워둘 수 없습니다.',
+            'required': '제목은 필수 항목입니다.'
+        }
+    )
+    content = serializers.CharField(
+        max_length=100,
+        error_messages={
+            'max_length': '내용은 100자를 초과할 수 없습니닷!',
+            'blank': '내용은 비워둘 수 없습니다.',
+            'required': '내용은 필수 항목입니다.'
+        }
+    )
